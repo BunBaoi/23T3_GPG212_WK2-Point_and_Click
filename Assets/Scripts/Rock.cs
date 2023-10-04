0e3cf62adc698adcfcfe91c9b850d03abbac4e82
BunBaoi <135566105+BunBaoi@users.noreply.github.com>
2023-10-04 11:07:32 +1100

    Main clicking and base game
    Done
    - Trigger response
    - True/False if block is alive
    - Health counter
    
    NEEDS
    - Now needs a respawn block
    - Other shops for upgrades
    - Increase Difficulty

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Rock : MonoBehaviour
 {
+    [Header("Health")]
+    public int counter = 0;
+    public int health = 10;
+
+    bool alive = true;
+
+    [Header("Gameobject Update")]
+    public TMP_Text HealthText;
+    public GameObject block;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        HealthText.text = "Health: " +  health.ToString();
+    }
+
+    public void OnMouseDown()
+    {
+        //counter += 1;
+        //Debug.Log("Count: " + counter);
+        health -= 1;
+        Debug.Log("Health: " + health + " ");
         
+        //Debug.Log(alive);
+        
+        if (health <= 0)
+        {
+            alive = false;
+            Destroy(gameObject);
+
+            Debug.Log(alive);
+        }
     }
 }
